2a24f236eb988f3ff840fe09d85adea93b1c52ae
Douglas Holanda <douglasholanda3195@gmail.com>
2024-01-16 19:59:31 -0300

    Meu primeiro commit

---
+++ rastreavel_agro/src/main/kotlin/douglas/rastreavel_agro/application/RastreavelAgroApplication.kt
@@ -0,0 +1,11 @@
+package douglas.rastreavel_agro.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RastreavelAgroApplication
+
+fun main(args: Array<String>) {
+	runApplication<RastreavelAgroApplication>(*args)
+}
---
+++ rastreavel_agro/src/test/kotlin/douglas/rastreavel_agro/RastreavelAgroApplicationTests.kt
@@ -0,0 +1,13 @@
+package douglas.rastreavel_agro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RastreavelAgroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
